50a8381f513b2903a26acfdb522090db3f3ce6fe
Kween <kweenyoallhiada@gmail.com>
2023-01-25 20:07:47 -0800

    Add example of imperative way to program in react

--- react/index.js
+++ react/index.js
@@ -3,7 +3,23 @@ the line of code you just learned! Nope, not on my watch!
 
 Try to write that 1-liner of React code again! This time,
 see if you can figure out how to render an <ul> with 2+ <li>s inside*/
-ReactDOM.render(
-    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
-    document.getElementById("root")
-)
+// ReactDOM.render(
+//     <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+//     document.getElementById("root")
+// )
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+
+const h1 = document.createElement("h1");
+h1.textContent = "This is an imperative way to program";
+h1.className = "header";
+document.getElementById("root").append(h1);
